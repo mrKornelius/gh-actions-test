6c6efc0f3fcaf5e6a0ee2f774e05f090e6105806
mrKornelius <magnus.korneliusson@gmail.com>
2024-10-13 21:50:38 +0200

    test

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [toggle, setToggle] = useState(false);
+  let x = 0;
+  useEffect(() => {
+    if (toggle) {
+      x = 2;
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{x}</Text>
       <StatusBar style="auto" />
     </View>
   );
