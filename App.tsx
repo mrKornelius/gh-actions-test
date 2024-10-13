a78dcf3a55d13a2464a8e337e2daaa5a87800fd4
mrKornelius <magnus.korneliusson@gmail.com>
2024-10-13 22:00:52 +0200

    formatting

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   const [toggle, setToggle] = useState(false);
